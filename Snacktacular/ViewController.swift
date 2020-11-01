ae7c75c053d1601c8dc4af788974da7b147dde37
John Pappas <johncpappas4@gmail.com>
2020-11-01 16:45:55 -0500

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by John Pappas on 11/1/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
