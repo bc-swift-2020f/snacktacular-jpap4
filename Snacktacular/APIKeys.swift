bd0aae75301cbd9f8653dfc30bbff89fc37fa95f
John Pappas <johncpappas4@gmail.com>
2020-11-02 14:27:15 -0500

    Completed Section 8.7

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by John Pappas on 11/2/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyAuyx4RvKDCNGcXovHq6TCbMylhWuiivfk"
+}
