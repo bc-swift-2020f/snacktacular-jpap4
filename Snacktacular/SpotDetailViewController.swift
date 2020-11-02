5810685d0dd5155d2b7f3a1c1a07abccc2ba7ea9
John Pappas <johncpappas4@gmail.com>
2020-11-01 19:23:52 -0500

    Completed Sections 8.1,8.2,8.3

---
+++ Snacktacular/SpotDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SpotDetailViewController.swift
+//  Snacktacular
+//
+//  Created by John Pappas on 11/1/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class SpotDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+        
+    }
+    
+}
