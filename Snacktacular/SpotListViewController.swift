c6a9e519331b8950c0601f4b87d4237cbf048ce0
John Pappas <johncpappas4@gmail.com>
2020-11-01 19:22:14 -0500

    Completed Section 8.3

---
+++ Snacktacular/SpotListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SpotListViewController.swift
+//  Snacktacular
+//
+//  Created by John Pappas on 11/1/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class SpotListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var spots = ["Islan Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+
+}
+
+extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotTableViewCell
+        cell.nameLabel?.text = spots[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+}
---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by John Pappas on 11/1/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratinglabel: UILabel!
+    
+
+
+}
