0e667469a1b6db2c8cbbca7ab4529750cc244d5b
John Pappas <johncpappas4@gmail.com>
2020-11-08 17:29:51 -0500

    Completed Section 8.16

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -20,10 +20,21 @@ class ReviewTableViewController: UITableViewController {
     @IBOutlet weak var reviewTextView: UITextView!
     @IBOutlet weak var deleteButton: UIButton!
     
+    @IBOutlet var starButtonCollection: [UIButton]!
+    
+    
     var review: Review!
     var spot: Spot!
-    
-    
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let imageName = (starButton.tag < rating ? "star.fill" : "star")
+                starButton.setImage(UIImage(systemName: imageName), for: .normal)
+                starButton.tintColor = (starButton.tag < rating ? .systemRed : .darkText)
+            }
+            review.rating = rating
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +54,7 @@ class ReviewTableViewController: UITableViewController {
         addressLabel.text = spot.address
         reviewTitleField.text = review.title
         reviewTitleField.text = review.text
+        rating = review.rating
         
     }
     
@@ -84,4 +96,8 @@ class ReviewTableViewController: UITableViewController {
         }
     }
     
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
+    }
+
 }
